1c84b575e875c748f527afa5cc68dca5db40a311
Egecan Akıncıoğlu <egecanakincioglu@outlook.com>
2025-03-31 20:18:41 +0200

    Removed development controls

--- assets/js/utils/navbar-toggle.js
+++ assets/js/utils/navbar-toggle.js
@@ -1,7 +1,6 @@
 const initNavbarToggle = () => {
     const toggleBtn = document.querySelector(".menu-toggle");
     const navMenu = document.querySelector(".nav-menu");
-    console.log("Navbar toggle loaded!");
   
     if (!toggleBtn || !navMenu) return;
   
